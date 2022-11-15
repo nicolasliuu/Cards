9dd600338918ef94a7f64d1193828366b9575d12
Nicolas <nicolasliu18@gmail.com>
2022-11-15 10:34:50 -0500

    Deck constructor and toString

--- Deck.java
+++ Deck.java
@@ -3,18 +3,21 @@ public class Deck {
     private Card[] cardsArray = new Card[52];
 
     public Deck() {
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 13; j++) {
-                cardsArray[(i + 1) * (j + 1) - 1] = new Card(j, i);
+        int suitCount = 1; 
+        for (int i = 1; i < cardsArray.length + 1; i++) {
+            cardsArray[i - 1] = new Card(i % 13, suitCount);
+            if (i % 13 == 0) {
+                cardsArray[i - 1] = new Card(13, suitCount);
+                suitCount += 1;
             }
         }
-    }
+    } 
 
     @Override
     public String toString() {
         String deckString = "";
         for (int i = 0; i < 52; i++) {
-            deckString = cardsArray[i].toString() + "\n";
+            deckString = deckString + cardsArray[i] + "\n"; //implicitly calls toString for cardsArray
         }
         return deckString;
     }
