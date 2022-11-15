58faa22f09122f88b69b1a026fb4a44a5ca83d54
nicolasliuu <nicolasliu18@gmail.com>
2022-11-15 00:56:31 -0500

    Task 1a

--- Card.java
+++ Card.java
@@ -3,5 +3,45 @@ public class Card {
    private int rank;
    private int suit;  
 
+   /**
+    * @param cardRank
+    * @param cardSuit
+    */
+   public Card (int cardRank, int cardSuit) {
+      if (cardRank < 1 || cardRank > 13 || cardSuit < 1 || cardSuit > 4) {
+         rank = 0;
+         suit = 0;
+      }
+      else {
+         rank = cardRank;
+         suit = cardSuit;
+      }
+   }
+
+   public int getRank() {
+      return rank;
+   }
+
+
+   
+   public int getSuit() {
+      return suit;
+   }
+
+   @Override
+   public String toString() {
+      String newString = "";
+      return newString;
+   }
+
+   @Override
+   public boolean equals(Object other) {
+      if (((Card)other).getRank() == getRank() && ((Card)other).getSuit() == getSuit()) {
+         return true;
+      }
+      else {
+         return false;
+      }
+   }
 
 }
---
+++ TestCard.java
@@ -0,0 +1,42 @@
+public class TestCard {
+    
+    public static void main(String[] args) {
+
+        // Constructing card object1 with valid parameters
+        Card card1 = new Card(1, 1);
+        // Constructing card object2 with valid rank, invalid suit
+        Card card2 = new Card(2, 0);
+        // Constructing card object3 with invalid rank, valid suit
+        Card card3 = new Card(-3, 2);
+        // Constructing card object4 with invalid rank and invalid suit
+        Card card4 = new Card(-123, 234);
+        // Getting rank and suit for object1
+        
+        System.out.println(card1.getSuit());
+        System.out.println(card1.getRank());
+
+        // Getting rank and suit for object2
+
+        System.out.println(card2.getSuit());
+        System.out.println(card2.getRank());
+
+        // Getting rank and suit for object3
+
+        System.out.println(card3.getSuit());
+        System.out.println(card3.getRank());
+
+        // Getting rank and suit for object4
+
+        System.out.println(card4.getSuit());
+        System.out.println(card4.getRank());
+
+        // Testing toString method by printing cards 1 through 4
+        
+        System.out.println(card1);
+        System.out.println(card2);
+        System.out.println(card3);
+        System.out.println(card4);
+
+    }
+
+}
